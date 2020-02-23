f0c644ddd7524d115d232d278efe27a98e97552a
guillaume-braibant <guillaume.braibant@nrb.be>
2020-02-23 11:39:28 +0000

    initial setup

---
+++ src/test/java/guillaumebraibant/springsoapwshellow/SpringSoapWsHellowApplicationTests.java
@@ -0,0 +1,13 @@
+package guillaumebraibant.springsoapwshellow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSoapWsHellowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
